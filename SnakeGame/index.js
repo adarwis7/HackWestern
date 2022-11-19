5830b711269c8c6389da2179e51d8b407f5e39f4
Ahmed Darwish <adarwis7@uwo.ca>
2022-11-19 15:52:59 -0500

    move to laptop

--- SnakeGame/index.js
+++ SnakeGame/index.js
@@ -4,3 +4,56 @@ const score = document.getElementById('score');
 const reset = document.getElementById('resetButton');
 const gameWidth = snakeBoard.width;
 const gameHeight = snakeBoard.height;
+const boardBackground = 'white';
+const snakeColor = 'red';
+const foodColor = 'green';
+const unitSize = 20;
+let running = false;
+let xSpeed = unitSize;
+let ySpeed = 0;
+let foodX;
+let foodY;
+let scoreVal = 0;
+
+// Snake
+let snake = [
+    { x: unitSize * 4, y: 0 },
+    { x: unitSize * 3, y: 0 },
+    { x: unitSize * 2, y: 0 },
+    { x: unitSize, y: 0 },
+    { x: 0, y: 0 }
+];
+
+window.addEventListener('keydown', changeDirection);
+reset.addEventListener('click', resetGame);
+
+function gameStart() {
+    if (!running) {
+        running = true;
+        draw();
+    }
+}
+function nextFrame() {
+    if (running) {
+        draw();
+    }
+}
+function resetGame() {
+    running = false;
+    xSpeed = unitSize;
+    ySpeed = 0;
+    snake = [
+        { x: unitSize * 4, y: 0 },
+        { x: unitSize * 3, y: 0 },
+        { x: unitSize * 2, y: 0 },
+        { x: unitSize, y: 0 },
+        { x: 0, y: 0 }
+    ];
+    scoreVal = 0;
+    score.innerHTML = scoreVal;
+    draw();
+}
+function drawFood() {
+    ctx.fillStyle = foodColor;
+    ctx.fillRect(foodX, foodY, unitSize, unitSize);
+}
