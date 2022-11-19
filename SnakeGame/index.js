c6f4dce169fafd39a718193139138825ccab4085
Ahmed Darwish <adarwis7@uwo.ca>
2022-11-19 00:48:23 -0500

    Skeleton for the html and some js

---
+++ SnakeGame/index.js
@@ -0,0 +1,6 @@
+const snakeBoard = document.getElementById('snakeBoard');
+const ctx = snakeBoard.getContext('2d');
+const score = document.getElementById('score');
+const reset = document.getElementById('resetButton');
+const gameWidth = snakeBoard.width;
+const gameHeight = snakeBoard.height;
